d813685cf9bc304e2042d241d31596826e767199
Heather Moreira <moreiraheather@gmail.com>
2020-03-26 23:25:27 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,9 @@
-def hello_t
+def hello_t(array)
+  i = 0
+  while i < array.length 
+    yield array[i]
+    i += 1 
+  end 
  ["Tim", "Tom", "Jim"].each do |name|
    if name.starts_with?("T")
      puts "Hi, #{name}"
